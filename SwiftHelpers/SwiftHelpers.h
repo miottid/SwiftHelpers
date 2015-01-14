5fdbb6ddf3ae1d9bea5f1fe98a76fea113a5bf46
David Miotti <david.miotti@wopata.com>
2015-01-14 11:31:23 +0100

    Initial Commit

---
+++ SwiftHelpers/SwiftHelpers.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHelpers.h
+//  SwiftHelpers
+//
+//  Created by David Miotti on 14/01/2015.
+//  Copyright (c) 2015 Wopata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftHelpers.
+FOUNDATION_EXPORT double SwiftHelpersVersionNumber;
+
+//! Project version string for SwiftHelpers.
+FOUNDATION_EXPORT const unsigned char SwiftHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHelpers/PublicHeader.h>
+
+
