7b30ff1fc569020e3b95c2dda06d21fa9c4a403e
David Miotti <david.miotti@wopata.com>
2015-03-23 16:10:42 +0100

    add UITextField extension: selectTextAtRange(range: NSRange)

---
+++ SwiftHelpers/UITextFieldExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UITextFieldExtension.swift
+//  SwiftHelpers
+//
+//  Created by David Miotti on 23/03/2015.
+//  Copyright (c) 2015 Wopata. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func selectTextAtRange(range: NSRange) {
+        if let start = positionFromPosition(beginningOfDocument, offset: range.location) {
+            if let end = positionFromPosition(start, offset: range.length) {
+                selectedTextRange = textRangeFromPosition(start, toPosition: end)
+            }
+        }
+    }
+}
