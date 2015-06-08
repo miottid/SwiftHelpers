df9e186bb993fd607f9a2d6c5108074cdc889430
David Miotti <david.miotti@wopata.com>
2015-06-08 14:00:19 +0200

    uicolor: add hex helpers

--- SwiftHelpers/UIColorExtension.swift
+++ SwiftHelpers/UIColorExtension.swift
@@ -45,4 +45,37 @@ public extension UIColor {
         return UIColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1.0)
     }
     
+    typealias RGBComponents = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    typealias HSBComponents = (hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat)
+    
+    var rgbComponents:RGBComponents {
+        var c:RGBComponents = (0,0,0,0)
+        
+        if getRed(&c.red, green: &c.green, blue: &c.blue, alpha: &c.alpha) {
+            return c
+        }
+        
+        return (0,0,0,0)
+    }
+    
+    var cssRGBA:String {
+        return String(format: "rgba(%d,%d,%d, %.02f)", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255), Int(rgbComponents.blue * 255), Float(rgbComponents.alpha))
+    }
+    var hexRGB:String {
+        return String(format: "#%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255), Int(rgbComponents.blue * 255))
+    }
+    var hexRGBA:String {
+        return String(format: "#%02x%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255), Int(rgbComponents.blue * 255), Int(rgbComponents.alpha * 255) )
+    }
+    
+    var hsbComponents:HSBComponents {
+        var c:HSBComponents = (0,0,0,0)
+        
+        if getHue(&c.hue, saturation: &c.saturation, brightness: &c.brightness, alpha: &c.alpha) {
+            return c
+        }
+        
+        return (0,0,0,0)
+    }
+    
 }
