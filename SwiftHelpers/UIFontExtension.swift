4131733f62aa913d70e415b37afbf8f1a9f9709c
David Miotti <david.miotti@wopata.com>
2015-06-16 11:40:27 +0200

    add UIFont: kernWithTracking

---
+++ SwiftHelpers/UIFontExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIFontExtension.swift
+//  SwiftHelpers
+//
+//  Created by David Miotti on 16/06/2015.
+//  Copyright (c) 2015 Wopata. All rights reserved.
+//
+
+import UIKit
+
+public extension UIFont {
+    
+    public func kernWithTracking(tracking: CGFloat) -> CGFloat {
+        return tracking * self.pointSize / 1000
+    }
+}
