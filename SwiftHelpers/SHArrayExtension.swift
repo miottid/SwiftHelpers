34fee55969dea4ca0f637a7dc5e8cb0790b78e21
David Miotti <david.miotti@wopata.com>
2016-04-25 17:03:53 +0200

    add shuffle array methods

--- SwiftHelpers/SHArrayExtension.swift
+++ SwiftHelpers/SHArrayExtension.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension Array {
+public extension Array {
     ///Loop through each item
     public func each(fn: (Element) -> ()) {
         for item in self {
@@ -21,4 +21,36 @@ extension Array {
 ///:params: array, the Array to loop from
 public func each(array: Array<AnyObject>, fn: ((AnyObject) -> ())) {
     array.each(fn)
+}
+
+
+public extension Array {
+    public var shuffle: [Element] {
+        var elements = self
+        for index in indices.dropLast() {
+            guard
+                case let swapIndex = Int(arc4random_uniform(UInt32(count - index))) + index
+                where swapIndex != index else { continue }
+            swap(&elements[index], &elements[swapIndex])
+        }
+        return elements
+    }
+    
+    public mutating func shuffled() {
+        for index in indices.dropLast() {
+            guard
+                case let swapIndex = Int(arc4random_uniform(UInt32(count - index))) + index
+                where swapIndex != index
+                else { continue }
+            swap(&self[index], &self[swapIndex])
+        }
+    }
+    
+    public var chooseOne: Element {
+        return self[Int(arc4random_uniform(UInt32(count)))]
+    }
+    
+    public func choose(n: Int) -> [Element] {
+        return Array(shuffle.prefix(n))
+    }
 }
