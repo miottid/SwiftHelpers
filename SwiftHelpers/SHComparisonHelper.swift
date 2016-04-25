8543c557dfbd41de8c4a8d7aaf731c48c245c6da
David Miotti <david.miotti@wopata.com>
2016-04-25 16:19:13 +0200

    add more comparable types

--- SwiftHelpers/SHComparisonHelper.swift
+++ SwiftHelpers/SHComparisonHelper.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+/// Int - Float operations
+
 public func == (lhs: Int, rhs: Float) -> Bool {
     return Float(lhs) == rhs
 }
@@ -20,12 +22,12 @@ public func < (lhs: Int, rhs: Float) -> Bool {
     return Float(lhs) < rhs
 }
 
-public func > (lhs: Int, rhs: Float) -> Bool {
-    return Float(lhs) > rhs
-}
-
 public func < (lhs: Float, rhs: Int) -> Bool {
     return rhs > lhs
+}
+
+public func > (lhs: Int, rhs: Float) -> Bool {
+    return Float(lhs) > rhs
 }
 
 public func > (lhs: Float, rhs: Int) -> Bool {
@@ -36,22 +38,268 @@ public func >= (lhs: Float, rhs: Int) -> Bool {
     return lhs == rhs || lhs > rhs
 }
 
-public func <= (lhs: Float, rhs: Int) -> Bool {
-    return lhs == rhs || lhs < rhs
-}
-
 public func >= (lhs: Int, rhs: Float) -> Bool {
     return lhs == rhs || lhs > rhs
+}
+
+public func <= (lhs: Float, rhs: Int) -> Bool {
+    return lhs == rhs || lhs < rhs
 }
 
 public func <= (lhs: Int, rhs: Float) -> Bool {
     return lhs == rhs || lhs < rhs
 }
 
+public func - (lhs: Float, rhs: Int) -> Float {
+    return lhs - Float(rhs)
+}
+
+public func + (lhs: Float, rhs: Int) -> Float {
+    return lhs + Float(rhs)
+}
+
+public func + (lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) + rhs
+}
+
 public func += (inout lhs: Float, rhs: Int) {
-    lhs = lhs + Float(rhs)
+    lhs = lhs + rhs
 }
 
 public func -= (inout lhs: Float, rhs: Int) {
-    lhs = lhs - Float(rhs)
+    lhs = lhs - rhs
+}
+
+public func / (lhs: Float, rhs: Int) -> Float {
+    return lhs / Float(rhs)
+}
+
+public func * (lhs: Float, rhs: Int) -> Float {
+    return lhs * Float(rhs)
+}
+
+public func / (lhs: Int, rhs: Float) -> Float {
+    return lhs / Float(rhs)
+}
+
+/// Double - Float operations
+
+public func == (lhs: Float, rhs: Double) -> Bool {
+    return Double(lhs) == rhs
+}
+
+public func == (lhs: Double, rhs: Float) -> Bool {
+    return rhs == lhs
+}
+
+public func < (lhs: Float, rhs: Double) -> Bool {
+    return Double(lhs) < rhs
+}
+
+public func < (lhs: Double, rhs: Float) -> Bool {
+    return rhs > lhs
+}
+
+public func > (lhs: Float, rhs: Double) -> Bool {
+    return Double(lhs) > rhs
+}
+
+public func > (lhs: Double, rhs: Float) -> Bool {
+    return rhs < lhs
+}
+
+public func >= (lhs: Double, rhs: Float) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func >= (lhs: Float, rhs: Double) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func <= (lhs: Double, rhs: Float) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func <= (lhs: Float, rhs: Double) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func - (lhs: Double, rhs: Float) -> Double {
+    return lhs - Double(rhs)
+}
+
+public func + (lhs: Double, rhs: Float) -> Double {
+    return lhs + Double(rhs)
+}
+
+public func + (lhs: Float, rhs: Double) -> Double {
+    return Double(lhs) + rhs
+}
+
+public func += (inout lhs: Double, rhs: Float) {
+    lhs = lhs + rhs
+}
+
+public func -= (inout lhs: Double, rhs: Float) {
+    lhs = lhs - rhs
+}
+
+public func / (lhs: Double, rhs: Float) -> Double {
+    return lhs / Double(rhs)
+}
+
+public func * (lhs: Double, rhs: Float) -> Double {
+    return lhs * Double(rhs)
+}
+
+public func / (lhs: Float, rhs: Double) -> Double {
+    return lhs / Double(rhs)
+}
+
+/// Int - Double operations
+
+public func == (lhs: Int, rhs: Double) -> Bool {
+    return Double(lhs) == rhs
+}
+
+public func == (lhs: Double, rhs: Int) -> Bool {
+    return rhs == lhs
+}
+
+public func < (lhs: Int, rhs: Double) -> Bool {
+    return Double(lhs) < rhs
+}
+
+public func < (lhs: Double, rhs: Int) -> Bool {
+    return rhs > lhs
+}
+
+public func > (lhs: Int, rhs: Double) -> Bool {
+    return Double(lhs) > rhs
+}
+
+public func > (lhs: Double, rhs: Int) -> Bool {
+    return rhs < lhs
+}
+
+public func >= (lhs: Double, rhs: Int) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func >= (lhs: Int, rhs: Double) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func <= (lhs: Double, rhs: Int) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func <= (lhs: Int, rhs: Double) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func - (lhs: Double, rhs: Int) -> Double {
+    return lhs - Double(rhs)
+}
+
+public func + (lhs: Double, rhs: Int) -> Double {
+    return lhs + Double(rhs)
+}
+
+public func + (lhs: Int, rhs: Double) -> Double {
+    return Double(lhs) + rhs
+}
+
+public func += (inout lhs: Double, rhs: Int) {
+    lhs = lhs + rhs
+}
+
+public func -= (inout lhs: Double, rhs: Int) {
+    lhs = lhs - rhs
+}
+
+public func / (lhs: Double, rhs: Int) -> Double {
+    return lhs / Double(rhs)
+}
+
+public func * (lhs: Double, rhs: Int) -> Double {
+    return lhs * Double(rhs)
+}
+
+public func / (lhs: Int, rhs: Double) -> Double {
+    return lhs / Double(rhs)
+}
+
+/// Int - CGFloat
+
+public func == (lhs: Int, rhs: CGFloat) -> Bool {
+    return CGFloat(lhs) == rhs
+}
+
+public func == (lhs: CGFloat, rhs: Int) -> Bool {
+    return rhs == lhs
+}
+
+public func < (lhs: Int, rhs: CGFloat) -> Bool {
+    return CGFloat(lhs) < rhs
+}
+
+public func < (lhs: CGFloat, rhs: Int) -> Bool {
+    return rhs > lhs
+}
+
+public func > (lhs: Int, rhs: CGFloat) -> Bool {
+    return CGFloat(lhs) > rhs
+}
+
+public func > (lhs: CGFloat, rhs: Int) -> Bool {
+    return rhs < lhs
+}
+
+public func >= (lhs: CGFloat, rhs: Int) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func >= (lhs: Int, rhs: CGFloat) -> Bool {
+    return lhs == rhs || lhs > rhs
+}
+
+public func <= (lhs: CGFloat, rhs: Int) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func <= (lhs: Int, rhs: CGFloat) -> Bool {
+    return lhs == rhs || lhs < rhs
+}
+
+public func - (lhs: CGFloat, rhs: Int) -> CGFloat {
+    return lhs - CGFloat(rhs)
+}
+
+public func + (lhs: CGFloat, rhs: Int) -> CGFloat {
+    return lhs + CGFloat(rhs)
+}
+
+public func + (lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) + rhs
+}
+
+public func += (inout lhs: CGFloat, rhs: Int) {
+    lhs = lhs + rhs
+}
+
+public func -= (inout lhs: CGFloat, rhs: Int) {
+    lhs = lhs - rhs
+}
+
+public func / (lhs: CGFloat, rhs: Int) -> CGFloat {
+    return lhs / CGFloat(rhs)
+}
+
+public func * (lhs: CGFloat, rhs: Int) -> CGFloat {
+    return lhs * CGFloat(rhs)
+}
+
+public func / (lhs: Int, rhs: CGFloat) -> CGFloat {
+    return lhs / CGFloat(rhs)
 }
