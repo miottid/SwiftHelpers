45f5faa54ec0236c04887e8b202b1c722a4ca10f
David Miotti <david.miotti@wopata.com>
2016-05-10 14:50:31 +0200

    add cleanValue to Float, CGFloat and Double

---
+++ SwiftHelpers/SHNumberExtension.swift
@@ -0,0 +1,27 @@
+//
+//  SHFloatExtension.swift
+//  SwiftHelpers
+//
+//  Created by David Miotti on 10/05/16.
+//  Copyright © 2016 Wopata. All rights reserved.
+//
+
+import Foundation
+
+public extension Float {
+    public var cleanValue: String {
+        return Double(self).cleanValue
+    }
+}
+
+public extension CGFloat {
+    public var cleanValue: String {
+        return Double(self).cleanValue
+    }
+}
+
+public extension Double {
+    public var cleanValue: String {
+        return self % 1 == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
