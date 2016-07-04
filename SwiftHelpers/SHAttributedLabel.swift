d88e3aa7c4ada189aadb9034fbbb065c43b010f3
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2016-07-04 11:52:23 +0200

    Added a UILabel subclass that handles the kerning and line spacing.

---
+++ SwiftHelpers/SHAttributedLabel.swift
@@ -0,0 +1,64 @@
+//
+//  SHAttributedLabel.swift
+//  SwiftHelpers
+//
+//  Created by Maxime de Chalendar on 04/07/2016.
+//  Copyright © 2016 Wopata. All rights reserved.
+//
+
+#if os(iOS)
+
+import UIKit
+
+/// A class that allows you to edit the line spacing and kerning of labels without having to deal with NSAttributedString
+@IBDesignable class AttributedLabel: UILabel {
+
+    /// The kerning (characters spacing) of the label
+    @IBInspectable var kerning: CGFloat = 0 {
+        didSet {
+            refreshAttributes()
+        }
+    }
+
+    /// The interline spacing (vertical spacing) of the label
+    @IBInspectable var interlineSpacing: CGFloat = 0 {
+        didSet {
+            refreshAttributes()
+        }
+    }
+
+    override var text: String? {
+        didSet {
+            refreshAttributes()
+        }
+    }
+
+    override var font: UIFont? {
+        didSet {
+            refreshAttributes()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        refreshAttributes()
+    }
+
+    private func refreshAttributes() {
+        var attributes: [String: AnyObject] = [:]
+        attributes[NSFontAttributeName] = font
+        if kerning > 0 {
+            attributes[NSKernAttributeName] = kerning
+        }
+        if interlineSpacing > 0 {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = textAlignment
+            paragraphStyle.lineSpacing = CGFloat(interlineSpacing)
+            attributes[NSParagraphStyleAttributeName] = paragraphStyle
+        }
+        attributedText = NSAttributedString(string: text ?? "", attributes: attributes)
+    }
+    
+}
+
+#endif
