01db7f44cfb6f998fa7ec365ac8668d8f466deff
dmiotti <david@muxumuxu.com>
2017-09-20 15:14:27 +0200

    [-] Fix setting delegate

---
+++ SwiftHelpers/Core/SHMailExtension.swift
@@ -0,0 +1,43 @@
+//
+//  SHMailExtension.swift
+//  SwiftHelpers
+//
+//  Created by David Miotti on 04/09/2017.
+//  Copyright © 2017 Wopata. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+public class SHMail {
+    public func sendMail(subject: String?, recipient: String, in controller: UIViewController) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            if let subject = subject {
+                mail.setSubject(subject)
+            }
+            mail.setToRecipients([recipient])
+            controller.present(mail, animated: true, completion: nil)
+            return
+        }
+        
+        if  let encodedSubject = subject?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+            let encodedEmail = recipient.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            
+            // Try googlemail://
+            let googleMail = "googlemail://co?subject=\(encodedSubject)&to=\(encodedEmail)"
+            if let url = URL(string: googleMail), UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.openURL(url)
+                return
+            }
+            
+            // Try inbox-gmail://
+            let inbox = "inbox-gmail://co?subject=\(encodedSubject)&to=\(encodedEmail)"
+            if let url = URL(string: inbox), UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.openURL(url)
+                return
+            }
+        }
+    }
+}
+
